39d036f2b812f193f5727bcfdcffb49bbcb9617d
chris-cozy <csande9@clemson.edu>
2023-09-05 16:08:16 -0400

    Added agent scema to models

---
+++ models/agent.js
@@ -0,0 +1,20 @@
+const mongoose = require("../config/database");
+
+const agentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  persona: {
+    type: String,
+    required: true,
+  },
+});
+
+const Agent = mongoose.model("Agent", agentSchema);
+
+module.exports = Agent;
