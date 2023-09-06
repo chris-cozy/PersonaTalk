f599123e880d2c0aae74ea8eb7ec0acfe68b1658
chris-cozy <csande9@clemson.edu>
2023-09-06 14:18:49 -0400

    Fixed route

--- routes/conversation.js
+++ routes/conversation.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 const conversationController = require("../controllers/conversationController");
 
-// GET /v1/conversation
-router.get("/", conversationController.getAllConversations);
-
 // GET /v1/conversation/:username/:agent_name
-router.get("/:username/:agent_name", conversationController.getConversation);
+router.get("/", conversationController.getConversation);
 
 // Add more routes as needed for conversation-related operations
 
