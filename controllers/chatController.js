bd1da307637463eb14c01171834584436c00ebba
chris-cozy <csande9@clemson.edu>
2023-09-06 11:27:18 -0400

    Validation checks for chat controller

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,9 +1,20 @@
 const chatService = require("../services/chatService");
+const User = require("../models/user");
+const Agent = require("../models/agent");
 
 // Create a response for the given message using conversation history
 async function generateChatResponse(req, res) {
   const { username, agent_name, message } = req.body;
+
   try {
+    const user = await User.findOne({ username: username });
+    const agent = await Agent.findOne({ agent_name: agent_name });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (!agent) {
+      return res.status(404).json({ message: "Agent not found" });
+    }
     const response = await chatService.generateResponse(
       username,
       agent_name,
