f594dc64bccc8476ec9746555317a67fb271d32c
chris-cozy <csande9@clemson.edu>
2023-09-07 16:50:30 -0400

    Updated API entry point to handle authentication

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require("express");
+const session = require("express-session");
+const uniqueIdentifier = require("../services/uniqueIdentifier");
+const passport = require("passport");
 const app = express();
 app.use(express.json());
+
+require("dotenv").config();
+
+// Initialize Passport.js and session middleware
+app.use(
+  session({
+    secret: uniqueIdentifier.generateSecretKey(32),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Include the Passport local configuration
+require("./middleware/passport-local");
 
 // Import route files
 const agentRoutes = require("./routes/agent");
 const userRoutes = require("./routes/user");
 const conversationRoutes = require("./routes/conversation");
 const chatRoutes = require("./routes/chat");
+const authRoutes = require("./routes/auth");
 
 // Mount routers onto specific paths
 app.use("/v1/agent", agentRoutes);
 app.use("/v1/user", userRoutes);
 app.use("/v1/conversation", conversationRoutes);
 app.use("/v1/chat", chatRoutes);
+app.use("/v1/auth", authRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
