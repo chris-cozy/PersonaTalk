fed068a4c4abf6d885fd6aa1e1981a5ad808f522
chris-cozy <csande9@clemson.edu>
2023-09-10 18:05:33 -0400

    Implemented funciton to check role for RBAC

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,20 @@ async function ensureAuthenticated(req, res, next) {
   res.status(500).json({ message: "User not logged in." });
 }
 
+async function checkRole([allowedRoles]) {
+  return (req, res, next) => {
+    // Check if user is authenticated and has the required role
+    if (req.isAuthenticated() && allowedRoles.includes(req.user.role)) {
+      return next(); // User has the required role, continue to the next middleware
+    }
+    // User is not authorized
+    return res
+      .status(403)
+      .json({ message: "Access denied, not enough clearance" });
+  };
+}
+
 module.exports = {
   ensureAuthenticated,
+  checkRole,
 };
