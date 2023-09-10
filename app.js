02b768942aa265fbc3cb4f2128baae6d0e541221
chris-cozy <csande9@clemson.edu>
2023-09-10 12:14:46 -0400

    Implemented service for generating the app secret

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const uniqueIdentifier = require("../services/uniqueIdentifier");
+const uniqueIdentifier = require("./services/uniqueIdentifier");
 const passport = require("passport");
 const app = express();
 app.use(express.json());
