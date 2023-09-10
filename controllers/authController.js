a2176a823bb64a9119e54b3a3da303b887b5847b
chris-cozy <csande9@clemson.edu>
2023-09-10 12:16:28 -0400

    Fixed register user method, added method to get current authenticated user

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,13 @@ const passport = require("passport");
 const User = require("../models/user");
 
 async function registerUser(req, res) {
+  console.log(req.body);
   const { username, password, display_name } = req.body;
-  const newUser = new User({ username: username, display_name: display_name });
+  const newUser = new User({
+    username: username,
+    password: password,
+    display_name: display_name,
+  });
 
   User.register(newUser, password, (err, user) => {
     if (err) {
@@ -15,6 +20,19 @@ async function registerUser(req, res) {
   });
 }
 
+async function getCurrentUser(req, res) {
+  // Check if the user is authenticated
+  if (req.isAuthenticated()) {
+    // The authenticated user's information is available in req.user
+    const currentUser = req.user;
+    res.json(currentUser);
+  } else {
+    // If the user is not authenticated, return an error or an appropriate response
+    res.status(401).json({ message: "Not authenticated" });
+  }
+}
+
 module.exports = {
   registerUser,
+  getCurrentUser,
 };
