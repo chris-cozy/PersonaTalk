8bc932b60cebe0d61a4e9674b30979cc9e73dbac
chris-cozy <csande9@clemson.edu>
2023-09-11 17:35:34 -0400

    Implemented specified conversation routes

--- routes/conversation.js
+++ routes/conversation.js
@@ -3,19 +3,52 @@ const router = express.Router();
 const conversationController = require("../controllers/conversationController");
 const { ensureAuthenticated, checkRole } = require("../middleware/auth");
 
-// GET /v1/conversation/:username/:agent_name
+// GET /v1/conversation
 router.get(
   "/",
   ensureAuthenticated,
-  checkRole(["user", "developer", "admin"]),
-  conversationController.getConversation
+  checkRole(["developer", "admin"]),
+  conversationController.getAllConversations
 );
 
+// DELETE /v1/conversation
 router.delete(
   "/",
   ensureAuthenticated,
+  checkRole(["developer", "admin"]),
+  conversationController.deleteAllConversations
+);
+
+// GET /v1/conversation/:username
+router.get(
+  "/:username",
+  ensureAuthenticated,
   checkRole(["user", "developer", "admin"]),
-  conversationController.dweleteConversation
+  conversationController.getUsersConversations
+);
+
+// DELETE /v1/conversation/:username
+router.delete(
+  "/:username",
+  ensureAuthenticated,
+  checkRole(["user", "developer", "admin"]),
+  conversationController.deleteUsersConversations
+);
+
+// GET /v1/conversation/:username/:agent_name
+router.get(
+  "/:username/:agent_name",
+  ensureAuthenticated,
+  checkRole(["user", "developer", "admin"]),
+  conversationController.getSpecificConversation
+);
+
+// DELETE /v1/conversation/:username/:agent_name
+router.delete(
+  "/:username/:agent_name",
+  ensureAuthenticated,
+  checkRole(["user", "developer", "admin"]),
+  conversationController.deleteSpecificConversation
 );
 
 module.exports = router;
